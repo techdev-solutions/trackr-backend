cc0612e51f8292fd3de66e07d64d1d98e5f8557c
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-12 17:10:56 +0100

    Fixed integration test config loading, added Javascript tests & resource loading to build.gradle

--- src/test/java/de/techdev/trackr/IntegrationTest.java
+++ src/test/java/de/techdev/trackr/IntegrationTest.java
@@ -1,5 +1,7 @@
 package de.techdev.trackr;
 
+import de.techdev.trackr.repository.JpaConfiguration;
+import de.techdev.trackr.security.SecurityConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,7 +12,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * @author Moritz Schulze
  */
 @WebAppConfiguration
-@ContextConfiguration(classes = {TrackrApplication.class})
+@ContextConfiguration(classes = {TrackrApplication.class, TrackrRootConfiguration.class, SecurityConfiguration.class, JpaConfiguration.class})
 @RunWith(SpringJUnit4ClassRunner.class)
 public abstract class IntegrationTest {
 }
