1dc32a4ac197ecbdbb4774f26c9403fef40f4410
Moritz Schulze <moritz.schulze@gmail.com>
2014-02-13 14:14:22 +0100

    Added grunt distribution to war build step Changed artifact name to ROOT.war

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO employee (id, firstName, lastName) VALUES (0, 'admin', 'admin');
-INSERT INTO credentials (id, email, enabled) VALUES (0, 'admin', 1);
+INSERT INTO credentials (id, email, enabled) VALUES (0, 'admin', true);
 INSERT INTO authority (id, authority) VALUES (0, 'ROLE_ADMIN');
 INSERT INTO authority (id, authority) VALUES (1, 'ROLE_STAFF');
 INSERT INTO authority (id, authority) VALUES (2, 'ROLE_EMPLOYEE');
