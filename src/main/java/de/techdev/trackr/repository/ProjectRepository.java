7398734f6fbfade472266314a77872e4df1354aa
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-03 12:56:15 +0100

    Added finder Project.findByIdentifier

--- src/main/java/de/techdev/trackr/repository/ProjectRepository.java
+++ src/main/java/de/techdev/trackr/repository/ProjectRepository.java
@@ -2,10 +2,13 @@ package de.techdev.trackr.repository;
 
 import de.techdev.trackr.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Moritz Schulze
  */
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    Project findByIdentifier(@Param("identifier") String identifier);
 
 }
