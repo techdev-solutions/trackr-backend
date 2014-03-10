4a0c24fb4d22fe69afe1adedc56f85c4ee495cbe
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-10 11:39:02 +0100

    Added finder method by project and time interval to BillableTimeRepository

--- src/main/java/de/techdev/trackr/repository/BillableTimeRepository.java
+++ src/main/java/de/techdev/trackr/repository/BillableTimeRepository.java
@@ -1,9 +1,14 @@
 package de.techdev.trackr.repository;
 
 import de.techdev.trackr.domain.BillableTime;
+import de.techdev.trackr.domain.Project;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author Moritz Schulze
@@ -20,4 +25,9 @@ public interface BillableTimeRepository extends CrudRepository<BillableTime, Lon
     @Override
     @PreAuthorize("hasRole('ROLE_SUPERVISOR')")
     BillableTime findOne(Long aLong);
+
+    @PreAuthorize("hasRole('ROLE_SUPERVISOR')")
+    List<BillableTime> findByProjectAndDateBetweenOrderByDateAsc(@Param("project") Project project,
+                                                                 @Param("start") Date start,
+                                                                 @Param("end") Date end);
 }
