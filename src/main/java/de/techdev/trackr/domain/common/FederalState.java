a26d2b07a9f97e97d72e29b0fa5a12fb2c8f8142
Moritz Schulze <moritz.schulze@gmail.com>
2014-03-14 10:33:23 +0100

    Fixed type

--- src/main/java/de/techdev/trackr/domain/common/FederalState.java
+++ src/main/java/de/techdev/trackr/domain/common/FederalState.java
@@ -10,7 +10,7 @@ public enum FederalState {
 
     BADEN_WUERTTEMBERG("Baden-Württemberg"), BAYERN("Bayern"), BERLIN("Berlin"), BRANDENBURG("Brandenburg"), BREMEN("Bremen"),
     HAMBURG("Hamburg"), HESSEN("Hessen"), MECKLENBURG_VORPOMMERN("Mecklenburg-Vorpommern"), NIEDERSACHSEN("Niedersachsen"), NORDRHEIN_WESTFALEN("Nordrhein-Westfalen"),
-    RHEINLAND_PFALZ("Rheinland-Pfalz"), SAARLAND("Saarland"), SACHSEN("Sachsen"), SACHSEN_ANHALT("Sachsen-Anhalt"), SCHLESWIG_HOLSTEINT("Schleswig-Holstein"), THUERINGEN("Thüringen");
+    RHEINLAND_PFALZ("Rheinland-Pfalz"), SAARLAND("Saarland"), SACHSEN("Sachsen"), SACHSEN_ANHALT("Sachsen-Anhalt"), SCHLESWIG_HOLSTEIN("Schleswig-Holstein"), THUERINGEN("Thüringen");
 
     private String state;
 
