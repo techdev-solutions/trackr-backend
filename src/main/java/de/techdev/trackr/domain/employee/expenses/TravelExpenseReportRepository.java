aa0062a3cef143d52af9ac9675b610d45dcd5525
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-08 21:29:35 +0200

    TRACKR-79 Work in progress frontend

--- src/main/java/de/techdev/trackr/domain/employee/expenses/TravelExpenseReportRepository.java
+++ src/main/java/de/techdev/trackr/domain/employee/expenses/TravelExpenseReportRepository.java
@@ -1,8 +1,13 @@
 package de.techdev.trackr.domain.employee.expenses;
 
+import de.techdev.trackr.domain.employee.Employee;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 /**
  * @author Moritz Schulze
@@ -16,4 +21,7 @@ public interface TravelExpenseReportRepository extends CrudRepository<TravelExpe
     @Override
     @PostAuthorize("hasRole('ROLE_SUPERVISOR') or ( isAuthenticated() and returnObject.employee.id == principal.id )")
     TravelExpenseReport findOne(Long aLong);
+
+    @PreAuthorize("isAuthenticated() and #employee.id == principal.id")
+    List<TravelExpenseReport> findByEmployee(@Param("employee") Employee employee);
 }
