6aa8397d7d1902b94a5066cdddd7255e63025fc3
Moritz Schulze <moritz.schulze@gmail.com>
2014-04-10 11:12:38 +0200

    Production build now completely rewrites the open_id.returnto url so trackr works behind a proxy.

--- src/main/java/de/techdev/trackr/core/security/OpenIDReturnToReplacementFilter.java
+++ src/main/java/de/techdev/trackr/core/security/OpenIDReturnToReplacementFilter.java
@@ -11,9 +11,10 @@ import java.io.IOException;
 /**
  * This filter will modify the url of the request in the case the application is currently processing an OpenID login. This has to be done so
  * it works behind a reverse proxy.
- *
+ * <p>
  * Normally one would overwrite {@link org.springframework.security.openid.OpenIDAuthenticationFilter#buildReturnToUrl(javax.servlet.http.HttpServletRequest)}, but
  * unfortunately this is not accessible via JavaConfig.
+ *
  * @author Moritz Schulze
  */
 @Slf4j
@@ -35,11 +36,16 @@ public class OpenIDReturnToReplacementFilter implements Filter {
         public java.lang.StringBuffer getRequestURL() {
             StringBuffer sb = super.getRequestURL();
 
-            int index = sb.indexOf("/login/openid");
-            if (index != -1 && !proxyPath.isEmpty()) {
-                log.debug("Changing the getRequestURL to inject the correct host so openid login could work behind proxy");
+            String openIdLoginPath = "/login/openid";
+            int index = sb.indexOf(openIdLoginPath);
+            if (index != -1) {
+                log.debug("Changing getRequestUrl so openid login works behind proxy.");
                 log.debug("Original getRequestURL: " + sb.toString());
-                sb.replace(index, sb.length(), "/" + proxyPath + "/login/openid");
+                if (proxyPath.startsWith("http")) {
+                    sb = new StringBuffer(proxyPath + openIdLoginPath);
+                } else if (!proxyPath.isEmpty()) {
+                    sb.replace(index, sb.length(), "/" + proxyPath + openIdLoginPath);
+                }
                 log.debug("New getRequestURL: " + sb.toString());
             }
             return sb;
