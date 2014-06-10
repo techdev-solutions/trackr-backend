06efb443f8fd77a0b13d9b20d810a6726f86c3fd
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-10 14:46:15 +0200

    TRACKR-101 - Add invoice Removed example module (we now have to modules) Added invoices module Created modal dialogue to add a new invoice
    TRACKR-106 - List invoices
    Added search by type to invoice repository
    Added tab display for invoices

--- src/main/java/de/techdev/trackr/domain/project/invoice/InvoiceRepository.java
+++ src/main/java/de/techdev/trackr/domain/project/invoice/InvoiceRepository.java
@@ -1,10 +1,15 @@
 package de.techdev.trackr.domain.project.invoice;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Moritz Schulze
  */
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    public Page<Invoice> findByInvoiceState(@Param("state") InvoiceState state, Pageable pageable);
 
 }
