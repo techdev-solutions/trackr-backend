98969bc3306eea935611d5b0a508cb849efca5d1
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-13 10:23:04 +0200

    TRACKR-110 - Use projections where possible / needed Invoice index now loads the debitor via a projection.

---
+++ src/main/java/de/techdev/trackr/domain/project/invoice/InvoiceWithDebitorProjection.java
@@ -0,0 +1,29 @@
+package de.techdev.trackr.domain.project.invoice;
+
+import de.techdev.trackr.domain.company.Company;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author Moritz Schulze
+ */
+@Projection(types = Invoice.class, name = "withDebitor")
+public interface InvoiceWithDebitorProjection {
+    Long getId();
+
+    Integer getVersion();
+
+    String getIdentifier();
+
+    Date getCreationDate();
+
+    BigDecimal getInvoiceTotal();
+
+    Company getDebitor();
+
+    Date getDueDate();
+
+    InvoiceState getInvoiceState();
+}
