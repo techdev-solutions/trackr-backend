96813d801fb6cec82ed435468305892f1eaed4b5
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-13 14:09:59 +0200

    TRACKR-93 - Use JNDI for Mail Resource Mail properties are now loaded via JNDI.

--- src/main/java/de/techdev/trackr/core/mail/MailConfiguration.java
+++ src/main/java/de/techdev/trackr/core/mail/MailConfiguration.java
@@ -2,16 +2,17 @@ package de.techdev.trackr.core.mail;
 
 import de.techdev.trackr.core.mail.support.JavaMailService;
 import de.techdev.trackr.core.mail.support.NoOpMailService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.PropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.Resource;
+import org.springframework.jndi.JndiAccessor;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.naming.NamingException;
 import java.util.Properties;
 
 /**
@@ -19,9 +20,6 @@ import java.util.Properties;
  */
 @Configuration
 public class MailConfiguration {
-
-    @Value("classpath:/mail/gmail.properties")
-    private Resource mailPropertiesResource;
 
     @Bean
     @Profile("prod")
@@ -36,15 +34,22 @@ public class MailConfiguration {
     }
 
     @Bean
+    @Profile("prod")
     public Properties mailProperties() {
-        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(mailPropertiesResource);
+        Properties mailProperties = new Properties();
+        JndiAccessor jndiAccessor = new JndiAccessor();
+        JndiTemplate jndiTemplate = jndiAccessor.getJndiTemplate();
+        String[] propertyNames = {"mail.smtp.host", "mail.smtp.port", "mail.smtp.auth", "mail.smtp.user",
+                "mail.smtp.password", "mail.smtp.starttls.enable", "mail.transport.protocol", "mail.smtp.socketFactory.class"};
         try {
-            propertiesFactoryBean.afterPropertiesSet();
-            return propertiesFactoryBean.getObject();
-        } catch (IOException e) {
-            throw new IllegalStateException("Could not open mail properties file", e);
+            for (String propertyName : propertyNames) {
+                Object propertyValue = jndiTemplate.lookup("java:comp/env/" + propertyName);
+                mailProperties.put(propertyName, propertyValue);
+            }
+        } catch (NamingException e) {
+            throw new IllegalStateException("Cannot read mail properties from tomcat environemt in context.xml", e);
         }
+        return mailProperties;
     }
 
     @Bean
@@ -54,7 +59,7 @@ public class MailConfiguration {
 
         /* Yes, thank you spring for this */
         javaMailSender.setHost(mailProperties().getProperty("mail.smtp.host"));
-        javaMailSender.setPort(Integer.valueOf(mailProperties().getProperty("mail.smtp.port")));
+        javaMailSender.setPort((Integer)mailProperties().get("mail.smtp.port"));
         javaMailSender.setUsername(mailProperties().getProperty("mail.smtp.user"));
         javaMailSender.setPassword(mailProperties().getProperty("mail.smtp.password"));
 
