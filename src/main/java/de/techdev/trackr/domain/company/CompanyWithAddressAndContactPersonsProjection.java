40731c0fd65adece62885ab0ba7021f798dfd623
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-19 11:54:08 +0200

    TRACKR-116 - Use forms and buttons rather than click-to-edit This commit includes a new general service to display a "new entity" or "update entity" form in a modal. The company create/edit dialogues have been converted to this new approach.

---
+++ src/main/java/de/techdev/trackr/domain/company/CompanyWithAddressAndContactPersonsProjection.java
@@ -0,0 +1,24 @@
+package de.techdev.trackr.domain.company;
+
+import org.springframework.data.rest.core.config.Projection;
+
+import java.util.List;
+
+/**
+ * @author Moritz Schulze
+ */
+@Projection(types = Company.class, name = "withAddressAndContactPersons")
+public interface CompanyWithAddressAndContactPersonsProjection {
+
+    Long getId();
+
+    Integer getVersion();
+
+    Long getCompanyId();
+
+    String getName();
+
+    Address getAddress();
+
+    List<ContactPerson> getContactPersons();
+}
