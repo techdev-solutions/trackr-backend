0cd41290079ad5f0d4b7fb7d21ca6b86160f02da
Moritz Schulze <moritz.schulze@gmail.com>
2014-06-27 14:32:17 +0200

    TRACKR-19 - Implement OAuth/other API authentication method Token store is now in a database for QS and Prod Removed the angular path Fixed test for LoginController

--- src/main/java/de/techdev/trackr/core/security/OAuth2ServerConfiguration.java
+++ src/main/java/de/techdev/trackr/core/security/OAuth2ServerConfiguration.java
@@ -1,14 +1,31 @@
 package de.techdev.trackr.core.security;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
+import static java.util.Arrays.asList;
 
 /**
  * @author Moritz Schulze
@@ -54,7 +71,32 @@ public class OAuth2ServerConfiguration {
 
     @Configuration
     @EnableAuthorizationServer
+    @PropertySource({"classpath:/META-INF/spring/database_${spring.profiles.active:dev}.properties"})
     protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Value("${tokenDatabase.driverClassName}")
+        private String dbDriver;
+
+        @Value("${tokenDatabase.url}")
+        private String dbUrl;
+
+        @Value("${tokenDatabase.username}")
+        private String username;
+
+        @Value("${tokenDatabase.password}")
+        private String password;
+
+        @Value("${tokenDatabase.jndiName}")
+        private String jndiName;
+
+        @Autowired
+        private Environment env;
+
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
+
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients.inMemory().withClient("trackr-page")
@@ -62,6 +104,36 @@ public class OAuth2ServerConfiguration {
                 .authorizedGrantTypes("authorization_code", "implicit") //TODO: what to set here?
                 .authorities("ROLE_CLIENT")
                 .scopes("read", "write");
+        }
+
+        @Bean
+        @Profile({"qs", "prod"})
+        public DataSource tokenDataSource() {
+            if (asList(env.getActiveProfiles()).contains("prod")) {
+                JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+                return lookup.getDataSource(jndiName);
+            } else {
+                BasicDataSource dataSource = new BasicDataSource();
+                dataSource.setDriverClassName(dbDriver);
+                dataSource.setUrl(dbUrl);
+                dataSource.setUsername(username);
+                dataSource.setPassword(password);
+                return dataSource;
+            }
+        }
+
+        @Bean
+        public TokenStore tokenStore() {
+            if(asList(env.getActiveProfiles()).contains("dev")) {
+                return new InMemoryTokenStore();
+            } else {
+                return new JdbcTokenStore(tokenDataSource());
+            }
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.tokenStore(tokenStore());
         }
     }
 }
