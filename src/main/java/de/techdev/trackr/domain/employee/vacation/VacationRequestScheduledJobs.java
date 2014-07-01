7088514cefe5c2200e6af78d55a39062f898aa0d
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-01 15:54:45 +0200

    TRACKR-60 - Automatically approve vacation request Added a log statement to the vacation request scheduled job.

--- src/main/java/de/techdev/trackr/domain/employee/vacation/VacationRequestScheduledJobs.java
+++ src/main/java/de/techdev/trackr/domain/employee/vacation/VacationRequestScheduledJobs.java
@@ -1,11 +1,13 @@
 package de.techdev.trackr.domain.employee.vacation;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author Moritz Schulze
  */
+@Slf4j
 public class VacationRequestScheduledJobs {
 
     @Autowired
@@ -13,6 +15,7 @@ public class VacationRequestScheduledJobs {
 
     @Scheduled(cron = "0 0 4 * * *")
     public void approveSevenDaysOldVacationRequests() {
+        log.debug("Executing vacation request approval trigger.");
         vacationRequestApproveService.approveSevenDayOldRequests();
     }
 
