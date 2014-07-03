d061e71d76e4156fadc5764618944e4d0e3056e5
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-03 10:57:17 +0200

    Fixed compile error

--- src/main/java/de/techdev/trackr/domain/project/WorkTimeWithEmployeeProjection.java
+++ src/main/java/de/techdev/trackr/domain/project/WorkTimeWithEmployeeProjection.java
@@ -1,6 +1,5 @@
 package de.techdev.trackr.domain.project;
 
-import com.sun.xml.internal.ws.api.pipe.FiberContextSwitchInterceptor;
 import de.techdev.trackr.domain.employee.Employee;
 import org.springframework.data.rest.core.config.Projection;
 
