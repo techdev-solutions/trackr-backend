df1c8aeace333dd5ae6aaac96ea283726aa4a7a9
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-04 14:28:12 +0200

    TRACKR-172 - Don't display the admin in the address book Simplified web endpoint for the address book (now uses @PageableDefault). Sort is now included. Filtered admin in the repository method as it conflicts with the use of the Page

--- src/main/java/de/techdev/trackr/domain/employee/addressbook/AddressBookController.java
+++ src/main/java/de/techdev/trackr/domain/employee/addressbook/AddressBookController.java
@@ -4,15 +4,14 @@ import de.techdev.trackr.domain.employee.Employee;
 import de.techdev.trackr.domain.employee.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resources;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -25,7 +24,6 @@ import java.util.stream.Collectors;
 @RequestMapping("/address_book")
 public class AddressBookController {
 
-    public static final String ADMIN_EMAIL = "admin@techdev.de";
     @Autowired
     private EmployeeRepository employeeRepository;
 
@@ -33,11 +31,10 @@ public class AddressBookController {
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, produces = "application/hal+json")
     public Resources<ReducedEmployee> getAddressList(
-            @RequestParam(value = "size", defaultValue = "10", required = false) int size,
-            @RequestParam(value = "page", defaultValue = "0", required = false) int page) {
-        Pageable pageable = new PageRequest(page, size);
-        Page<Employee> pageOfEmployees = employeeRepository.findAllNotExported(pageable);
-        List<ReducedEmployee> reducedEmployees = transformToReducedEmployeesAndRemoveAdmin(pageOfEmployees.getContent());
+            @PageableDefault(sort = "lastName") Pageable pageable
+    ) {
+        Page<Employee> pageOfEmployees = employeeRepository.findAllForAddressBook(pageable);
+        List<ReducedEmployee> reducedEmployees = transformToReducedEmployees(pageOfEmployees.getContent());
         return new PagedResources<ReducedEmployee>(reducedEmployees, pageMetadataFromPage(pageOfEmployees));
     }
 
@@ -47,11 +44,10 @@ public class AddressBookController {
      * @param listOfEmployees The list to transform
      * @return The transformed list.
      */
-    protected List<ReducedEmployee> transformToReducedEmployeesAndRemoveAdmin(List<Employee> listOfEmployees) {
+    protected List<ReducedEmployee> transformToReducedEmployees(List<Employee> listOfEmployees) {
         return listOfEmployees
                 .stream()
                 .map(ReducedEmployee::valueOf)
-                .filter( e -> !ADMIN_EMAIL.equals(e.getEmail()))
                 .collect(Collectors.toList());
     }
 
--- src/test/java/de/techdev/trackr/domain/employee/addressbook/AddressBookControllerTest.java
+++ src/test/java/de/techdev/trackr/domain/employee/addressbook/AddressBookControllerTest.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.echocat.jomon.testing.BaseMatchers.isNotEmpty;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 public class AddressBookControllerTest {
@@ -23,31 +21,12 @@ public class AddressBookControllerTest {
     }
 
     @Test
-    public void transformToReducedEmployeesAndRemoveAdminRemovesAdmin() throws Exception {
-        Credential adminCredential = new Credential();
-        adminCredential.setEmail(AddressBookController.ADMIN_EMAIL);
-        Employee admin = new Employee();
-        admin.setCredential(adminCredential);
-
-        Credential normalCredential = new Credential();
-        normalCredential.setEmail("user@techdev.de");
-        Employee normal = new Employee();
-        normal.setCredential(normalCredential);
-
-        List<ReducedEmployee> reducedEmployees = addressBookController.transformToReducedEmployeesAndRemoveAdmin(asList(admin, normal));
-        for (ReducedEmployee reducedEmployee : reducedEmployees) {
-            assertThat(reducedEmployee.getEmail(), is(not(AddressBookController.ADMIN_EMAIL)));
-        }
-        assertThat(reducedEmployees, isNotEmpty());
-    }
-
-    @Test
     public void transformEmployees() throws Exception {
         Employee employee = new Employee();
         Credential credential = new Credential();
         employee.setCredential(credential);
         List<Employee> listOfEmployees = asList(employee);
-        List<ReducedEmployee> reducedEmployees = addressBookController.transformToReducedEmployeesAndRemoveAdmin(listOfEmployees);
+        List<ReducedEmployee> reducedEmployees = addressBookController.transformToReducedEmployees(listOfEmployees);
         assertThat(reducedEmployees, isNotEmpty());
     }
 }
