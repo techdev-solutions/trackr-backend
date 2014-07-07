7a43f88cb32c0d083eb7f48e1a4744c9770ddc89
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-07 14:16:17 +0200

    Generified how to get all supervisor email addresses.

--- src/main/java/de/techdev/trackr/domain/ApiBeansConfiguration.java
+++ src/main/java/de/techdev/trackr/domain/ApiBeansConfiguration.java
@@ -3,6 +3,7 @@ package de.techdev.trackr.domain;
 import de.techdev.trackr.domain.employee.expenses.TravelExpenseReportService;
 import de.techdev.trackr.domain.employee.expenses.support.TravelExpenseReportServiceImpl;
 import de.techdev.trackr.domain.employee.login.DeactivateEmployeesService;
+import de.techdev.trackr.domain.employee.login.support.SupervisorService;
 import de.techdev.trackr.domain.employee.vacation.HolidayCalculator;
 import de.techdev.trackr.domain.employee.vacation.VacationRequestApproveService;
 import de.techdev.trackr.domain.employee.vacation.support.VacationRequestApproveServiceImpl;
@@ -65,5 +66,10 @@ public class ApiBeansConfiguration {
     @Bean
     public ChangeStateService changeStateService() {
         return new ChangeStateService();
+    }
+
+    @Bean
+    public SupervisorService supervisorService() {
+        return new SupervisorService();
     }
 }
--- src/test/java/de/techdev/trackr/domain/employee/vacation/support/VacationRequestNotifyServiceIntegrationTest.java
+++ src/test/java/de/techdev/trackr/domain/employee/vacation/support/VacationRequestNotifyServiceIntegrationTest.java
@@ -6,13 +6,9 @@ import de.techdev.trackr.domain.ApiBeansConfiguration;
 import de.techdev.trackr.domain.employee.vacation.VacationRequest;
 import de.techdev.trackr.domain.employee.vacation.VacationRequestDataOnDemand;
 import de.techdev.trackr.domain.employee.vacation.VacationRequestStatus;
-import de.techdev.trackr.domain.employee.vacation.support.VacationRequestNotifyService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-
-import static org.echocat.jomon.testing.BaseMatchers.isNotEmpty;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * @author Moritz Schulze
@@ -38,11 +34,5 @@ public class VacationRequestNotifyServiceIntegrationTest extends IntegrationTest
         VacationRequest vacationRequest = vacationRequestDataOnDemand.getRandomObject();
         vacationRequest.setStatus(VacationRequestStatus.REJECTED);
         vacationRequestNotifyService.sendEmailNotification(vacationRequest);
-    }
-
-    @Test
-    public void collectSupervisorAddresses() throws Exception {
-        String[] supervisorEmailsConcatenated = vacationRequestNotifyService.getSupervisorEmailsConcatenated();
-        assertThat(supervisorEmailsConcatenated, isNotEmpty());
     }
 }
