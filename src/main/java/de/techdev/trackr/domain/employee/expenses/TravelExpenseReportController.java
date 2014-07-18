0fa5f9e4ad6ace83e2a1e579ca0200e847b449a3
Moritz Schulze <moritz.schulze@gmail.com>
2014-07-18 16:14:14 +0200

    TRACKR-187 - Export travel expense reports to PDF Designed travel expense report pdf

--- src/main/java/de/techdev/trackr/domain/employee/expenses/TravelExpenseReportController.java
+++ src/main/java/de/techdev/trackr/domain/employee/expenses/TravelExpenseReportController.java
@@ -8,11 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.context.Context;
 
 import javax.servlet.http.HttpServletResponse;
+import java.math.BigDecimal;
 import java.security.Principal;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Moritz Schulze
@@ -26,6 +31,9 @@ public class TravelExpenseReportController {
 
     @Autowired
     private PdfRenderer pdfRenderer;
+
+    @Autowired
+    private TravelExpenseReportRepository travelExpenseReportRepository;
 
     @RequestMapping(value = "/{id}/submit", method = RequestMethod.PUT)
     @ResponseBody
@@ -50,9 +58,24 @@ public class TravelExpenseReportController {
 
     @PreAuthorize("hasRole('ROLE_SUPERVISOR')")
     @RequestMapping(value = "/{id}/pdf", produces = "application/pdf")
+    @Transactional
     public ResponseEntity<byte[]> asPdf(@PathVariable("id") TravelExpenseReport travelExpenseReport) {
+        TravelExpenseReport report = travelExpenseReportRepository.findOne(travelExpenseReport.getId());
+
         Context ctx = new Context();
-        ctx.setVariable("report", travelExpenseReport);
+        ctx.setVariable("report", report);
+        ctx.setVariable("today", new Date());
+        List<TravelExpense> expenses = report.getExpenses();
+        BigDecimal totalCost = expenses.stream().map(TravelExpense::getCost).reduce(BigDecimal.ZERO, (b1, b2) -> b1.add(b2));
+        ctx.setVariable("totalCost", totalCost);
+        Optional<Date> startDate = expenses.stream().map(TravelExpense::getFromDate).min(Date::compareTo);
+        Optional<Date> endDate = expenses.stream().map(TravelExpense::getToDate).max(Date::compareTo);
+        if (startDate.isPresent()) {
+            ctx.setVariable("startDate", startDate.get());
+        }
+        if(endDate.isPresent()) {
+            ctx.setVariable("endDate", endDate.get());
+        }
         try {
             byte[] pdf = pdfRenderer.renderPdf("travel-expenses/report", ctx);
             byte[] pdfBase64 = Base64.encodeBase64(pdf);
