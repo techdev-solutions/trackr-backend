75f4ea34cc7a144622076ada0037b94bbe4072e1
Moritz Schulze <moritz.schulze@techdev.de>
2015-06-04 11:17:03 +0200

    TRACKR-234 - Track employee's contact address Made the flyway migration script H2 compatible.

--- src/main/resources/db/migration/V19__add_employee_address.sql
+++ src/main/resources/db/migration/V19__add_employee_address.sql
@@ -1,1 +1,2 @@
-ALTER TABLE employee ADD COLUMN address_id int8 REFERENCES address;
+ALTER TABLE employee ADD COLUMN address_id int8;
+ALTER TABLE employee ADD CONSTRAINT employee_contact_address FOREIGN KEY (address_id) REFERENCES address;
