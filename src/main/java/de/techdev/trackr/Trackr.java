d0d1df078dd2c5558236634830134f695d2785cf
Moritz Schulze <moritz.schulze@techdev.de>
2016-03-24 13:59:27 +0100

    TRACKR-297 Fixes GMail Configuration
    Spring Boot 1.3.3 introduced a circular dependency of beans in trackr.
    The Spring Mail autoconfiguration autowired a session and exposed a JavaMailSender
    while our configuration autowired the JavaMailSender and exposed the session.
    
    We need the session for the Spring Integration configuration as a bean. The solution
    is to create the JavaMailSender ourselves. This is done by copying the relevant parts
    out of the Spring auto configuration and disabling it.

--- src/main/java/de/techdev/trackr/Trackr.java
+++ src/main/java/de/techdev/trackr/Trackr.java
@@ -4,17 +4,18 @@ import org.springframework.boot.ApplicationPid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MailSenderAutoConfiguration.class)
 @ImportResource(value = "classpath:META-INF/mail-integration.xml")
 public class Trackr {
 
--- src/main/java/de/techdev/trackr/core/mail/GMailConfiguration.java
+++ src/main/java/de/techdev/trackr/core/mail/GMailConfiguration.java
@@ -1,31 +1,66 @@
 package de.techdev.trackr.core.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Map;
+import java.util.Properties;
 import javax.mail.Session;
 
+/**
+ * This configuration class is mostly copied from {@link org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration}.
+ *
+ * Since we need to expose the {@link #mailSession()} for our {@code mail-integration.xml} to work and the Spring autoconfig
+ * class creates a circular dependency (JavaMailSender <-> Session) we have to use the relevant parts ourselves.
+ */
 @Configuration
+@EnableConfigurationProperties(MailProperties.class)
 @Profile("gmail")
 public class GMailConfiguration {
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private MailProperties properties;
+
+    @Bean
+    public JavaMailSenderImpl mailSender() {
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        applyProperties(sender);
+        return sender;
+    }
+
+    private void applyProperties(JavaMailSenderImpl sender) {
+        sender.setHost(this.properties.getHost());
+        if (this.properties.getPort() != null) {
+            sender.setPort(this.properties.getPort());
+        }
+        sender.setUsername(this.properties.getUsername());
+        sender.setPassword(this.properties.getPassword());
+        sender.setProtocol(this.properties.getProtocol());
+        if (this.properties.getDefaultEncoding() != null) {
+            sender.setDefaultEncoding(this.properties.getDefaultEncoding().name());
+        }
+        if (!this.properties.getProperties().isEmpty()) {
+            sender.setJavaMailProperties(asProperties(this.properties.getProperties()));
+        }
+    }
+
+    private Properties asProperties(Map<String, String> source) {
+        Properties properties = new Properties();
+        properties.putAll(source);
+        return properties;
+    }
 
     /**
      * We expose the mail session as a bean for the Spring Integration mail receiver.
      */
     @Bean
     public Session mailSession() {
-        if (JavaMailSenderImpl.class.isAssignableFrom(javaMailSender.getClass())) {
-            return ((JavaMailSenderImpl) javaMailSender).getSession();
-        } else {
-            throw new IllegalStateException("Mail sender must be a JavaMailSenderImpl");
-        }
+        return mailSender().getSession();
     }
 
 }
