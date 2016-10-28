220470bb092ddbf07426f9633ce075dc16ba514f
Alexander Hanschke <alexander.hanschke@gmail.com>
2016-10-28 17:02:44 +0200

    Update test annotations
    Spring Boot 1.4 introduces new annotations for testing and in the
    process deprecates some old ones. See here
    https://spring.io/blog/2016/07/28/spring-boot-1-4-released#test-improvem
    ents for more details.

--- src/test/java/de/techdev/test/TransactionalIntegrationTest.java
+++ src/test/java/de/techdev/test/TransactionalIntegrationTest.java
@@ -2,19 +2,19 @@ package de.techdev.test;
 
 import de.techdev.trackr.Trackr;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * A test that should rollback all transactions after finishing.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Trackr.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Trackr.class)
 @ActiveProfiles(value = {"in-memory-database"})
 @Transactional
-@TransactionConfiguration
+@Rollback
 public abstract class TransactionalIntegrationTest {
 }
